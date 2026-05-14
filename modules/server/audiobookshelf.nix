6d0293a3db31df066b959036d4d9298644b91001
TheGoatPrime234 <cato.jenisch@gmail.com>
2026-05-14 22:31:47 +0200

    Auto-Rebuild: 2026-05-14 22:31:47

--- modules/server/audiobookshelf.nix
+++ modules/server/audiobookshelf.nix
@@ -22,6 +22,7 @@
     services = {
       audiobookshelf = {
         enable = true;
+        package = pkgs-unstable.audiobookshelf;
         host = "0.0.0.0";
         port = 13378;
         openFirewall = true;
