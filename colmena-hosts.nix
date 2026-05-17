c4dcdb7d875b053221c0b38e08d62cd0b9fdd0d9
TheGoatPrime234 <cato.jenisch@gmail.com>
2026-05-17 15:01:39 +0200

    Auto-Rebuild: 2026-05-17 15:01:39

---
+++ colmena-hosts.nix
@@ -0,0 +1,60 @@
+{
+  inputs,
+  systemarch,
+  taruser,
+  commonSSHKeys,
+  pkgs-new,
+  pkgs-unstable,
+  ...
+}: {
+  meta = {
+    nixpkgs = import nixpkgs {
+      system = systemarch;
+      config.allowUnfree = true;
+      purity = "impure";
+    };
+    nodeNixpkgs = {
+      vicuna = import nixpkgs {
+        system = "aarch64-linux";
+        config.allowUnfree = true;
+      };
+    };
+    specialArgs = {inherit inputs pkgs-new pkgs-unstable;};
+  };
+  xeravus = {
+    deployment = {
+      targetHost = null;
+      allowLocalDeployment = true;
+      buildOnTarget = true;
+    };
+    imports = [
+      ./profiles/ssh-keys.nix
+      ./hosts/xeravus/configuration.nix
+    ];
+  };
+  xorus = {
+    deployment = {
+      targetHost = "192.168.178.69";
+      targetUser = taruser;
+      buildOnTarget = false;
+      keys = commonSSHKeys;
+    };
+    imports = [
+      ./profiles/ssh-keys.nix
+      ./hosts/xorus/configuration.nix
+    ];
+  };
+  vicuna = {
+    deployment = {
+      targetHost = "192.168.178.30";
+      targetUser = taruser;
+      buildOnTarget = false;
+      keys = commonSSHKeys;
+    };
+    imports = [
+      ./profiles/ssh-keys.nix
+      inputs.nixos-hardware.nixosModules.raspberry-pi-5
+      ./hosts/vicuna/configuration.nix
+    ];
+  };
+}
--- flake.nix
+++ flake.nix
@@ -96,57 +96,8 @@
         ];
       };
     };
-    colmena = {
-      meta = {
-        nixpkgs = import nixpkgs {
-          system = systemarch;
-          config.allowUnfree = true;
-          purity = "impure";
-        };
-        nodeNixpkgs = {
-          vicuna = import nixpkgs {
-            system = "aarch64-linux";
-            config.allowUnfree = true;
-          };
-        };
-        specialArgs = {inherit inputs pkgs-new pkgs-unstable;};
-      };
-      xeravus = {
-        deployment = {
-          targetHost = null;
-          allowLocalDeployment = true;
-          buildOnTarget = true;
-        };
-        imports = [
-          ./profiles/ssh-keys.nix
-          ./hosts/xeravus/configuration.nix
-        ];
-      };
-      xorus = {
-        deployment = {
-          targetHost = "192.168.178.69";
-          targetUser = taruser;
-          buildOnTarget = false;
-          keys = commonSSHKeys;
-        };
-        imports = [
-          ./profiles/ssh-keys.nix
-          ./hosts/xorus/configuration.nix
-        ];
-      };
-      vicuna = {
-        deployment = {
-          targetHost = "192.168.178.30";
-          targetUser = taruser;
-          buildOnTarget = false;
-          keys = commonSSHKeys;
-        };
-        imports = [
-          ./profiles/ssh-keys.nix
-          inputs.nixos-hardware.nixosModules.raspberry-pi-5
-          ./hosts/vicuna/configuration.nix
-        ];
-      };
+    colmena = import ./colmena-hosts.nix {
+      inherit inputs systemarch taruser commonSSHKeys pkgs-new pkgs-unstable;
     };
   };
 }
